fe3b4ba8d8a0f717310d7f2285135271ba4e364e
atolat <atolat@uci.edu>
2016-07-11 14:50:32 -0500

    First rollout

--- routes/server.js
+++ routes/server.js
@@ -40,18 +40,14 @@ app.post("/formdata", function (req, res) {
 
                 to: '+19493000798', // Any number Twilio can deliver to
                 from: '+19492200716', // A number you bought from Twilio and can use for outbound communication
-                body: name+'\n'+email+'\n'+phnum // body of the SMS message
+                body: 'Name: '=name+'\nEmail: '+email+'\nPhone Number: '+phnum // body of the SMS message
 
             }, function (err, responseData) { //this function is executed when a response is received from Twilio
 
                 if (!err) { // "err" is an error received during the request, if any
 
-                    // "responseData" is a JavaScript object containing data received from Twilio.
-                    // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
-                    // http://www.twilio.com/docs/api/rest/sending-sms#example-1
-
-                    console.log(responseData.from); // outputs "+14506667788"
-                    console.log(responseData.body); // outputs "word to your mother."
+                    console.log(responseData.from); 
+                    console.log(responseData.body); 
 
                 }
             });
