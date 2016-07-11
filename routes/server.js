84f8080efa814140e071b665d26db2909d415555
atolat <atolat@uci.edu>
2016-07-11 11:53:07 -0500

    Added form handler

--- routes/server.js
+++ routes/server.js
@@ -1,17 +1,18 @@
-    module.exports = function (app) {
+module.exports = function (app) {
 
-        var _ = require('underscore');
-        var bodyParser = require('body-parser');
-        app.use(bodyParser.json());
-        
-        //Twilio Integration
-        var client = require('twilio')('ACd54cb6f1b8a8bf9d23fe511d24d3459e', '472205f35904bda6943ed88a1343e2b1');
-        var twilio = require('twilio');
-        
-        //API ROOT
-        app.get('/', function (req, res) {
-            res.send('API Root');
-        });
+    var _ = require('underscore');
+    var bodyParser = require('body-parser');
+    app.use(bodyParser.json());
 
-        
+    //Twilio Integration
+    var client = require('twilio')('ACd54cb6f1b8a8bf9d23fe511d24d3459e', '472205f35904bda6943ed88a1343e2b1');
+    var twilio = require('twilio');
+
+    //API ROOT
+    app.get('/', function (req, res) {
+        res.send('API Root');
+    });
+
+    app.post('/formdata', function (req, res) {
+        res.json(req);
     }
--- test.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-// this is just a test
-//send back to the ajax request the request
-
-echo json_encode($_POST);?>
