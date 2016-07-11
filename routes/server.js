bf04e20e64e82d5218f5020c4a89d2176ad30691
atolat <atolat@uci.edu>
2016-07-11 14:01:18 -0500

    Formidable...

--- routes/server.js
+++ routes/server.js
@@ -4,7 +4,8 @@ module.exports = function (app) {
     var bodyParser = require('body-parser');
     app.use(bodyParser.json());
     var formidable = require('express-formidable');
-    app.use(formidable.parse())
+    app.use(formidable.parse());
+    util = require('util');
 
     //Twilio Integration
     var client = require('twilio')('ACd54cb6f1b8a8bf9d23fe511d24d3459e', '472205f35904bda6943ed88a1343e2b1');
@@ -17,6 +18,13 @@ module.exports = function (app) {
     
 
     app.post('/formdata', function (req, res) {
-        console.log(req.body);
+        var form = new formidable.IncomingForm();
+
+    form.parse(req, function(err, fields, files) {
+      res.writeHead(200, {'content-type': 'text/plain'});
+      res.write('received upload:\n\n');
+      res.end(util.inspect({fields: fields, files: files}));
+    });
+
     });
     }
