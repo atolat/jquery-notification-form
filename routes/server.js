86e1e0db1001e69f1d291e80c69580f8cad5eaf2
atolat <atolat@uci.edu>
2016-07-11 14:40:31 -0500

    Fingers Crossed...

--- routes/server.js
+++ routes/server.js
@@ -26,9 +26,35 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 app.post("/formdata", function (req, res) {
+    
     console.log(req.body.user.name);
     console.log(req.body.user.email);
     console.log(req.body.user.phnum);
+    
+    var name = req.body.user.name;
+    var email = req.body.user.email;
+    var phnum = req.body.user.phnum;
+    
+    
+    client.sendMessage({
+
+                to: '+19493000798', // Any number Twilio can deliver to
+                from: '+19492200716', // A number you bought from Twilio and can use for outbound communication
+                body: name+'\n'+email+'\n'+phnum // body of the SMS message
+
+            }, function (err, responseData) { //this function is executed when a response is received from Twilio
+
+                if (!err) { // "err" is an error received during the request, if any
+
+                    // "responseData" is a JavaScript object containing data received from Twilio.
+                    // A sample response from sending an SMS message is here (click "JSON" to see how the data appears in JavaScript):
+                    // http://www.twilio.com/docs/api/rest/sending-sms#example-1
+
+                    console.log(responseData.from); // outputs "+14506667788"
+                    console.log(responseData.body); // outputs "word to your mother."
+
+                }
+            });
   
 });
 
